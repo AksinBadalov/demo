ad56e829ea1c673b4dfcc3f5e316034f18d6a3d8
AksinBadalov <aksinbdlov@gmail.com>
2023-05-07 15:39:20 +0400

    first commit

---
+++ pages/aksin-1.js
@@ -0,0 +1,56 @@
+alert("Salam Aksin");
+console.log("salam");
+let x = 7;
+let y = 8;
+toplam = x + y;
+console.log(toplam);
+
+var ad = "Aksin";
+normalyazilis = ad;
+console.log("yazimin normal yazilmis vezyeti :" + normalyazilis);
+var ad = "Aksin";
+kicikyazilis = ad.toLocaleLowerCase();
+console.log("yazimin kicik yazilmis vezyeti :" + kicikyazilis);
+var ad = "Aksin";
+boyukyazilis = ad.toUpperCase();
+console.log("yazimin kicik yazilmis vezyeti :" + boyukyazilis);
+
+let k = 15;
+reqem = k;
+if (reqem > 15) {
+  console.log("Reqemim 15-den boyukdur");
+} else if (reqem < 15) {
+  console.log("Reqemim 15-den kicikdir");
+} else {
+  console.log("Reqemim 15-e beraberdir");
+}
+
+// var name='Memedaga';
+// var surname="Memedagaovic";
+// var nameeee=nameSurname;
+// console.log(nameeee);
+
+let text = "coders";
+console.log(text.includes("r"));
+
+let futbol=["Messi","Ronaldo","Neymar"];
+
+let s=7;
+let z=16;
+toplam=z+s;
+console.log(toplam);
+
+let degisken =toplam>0;
+for(degisken>0;degisken<30;degisken++)
+{
+  if(degisken>0){
+    console.log("Ededimiz musbetdir");
+  }
+  else if(degisken=0){
+    console.log("Ededimiz 0-a beraberdir");
+  }
+  else{
+    console.log("Ededimiz menfidir");
+  }
+}
+
